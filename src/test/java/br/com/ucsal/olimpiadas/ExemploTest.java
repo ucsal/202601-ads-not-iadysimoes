3d0fc477f6ec77c776528a769511508d15f2ab6d
Iady Simões <iady.sousa@ucsal.edu.br>
2026-03-31 03:26:34 -0300

    Refatoração e ajustes no sistema

--- src/test/java/br/com/ucsal/olimpiadas/ExemploTest.java
+++ src/test/java/br/com/ucsal/olimpiadas/ExemploTest.java
@@ -1,7 +1,7 @@
 package br.com.ucsal.olimpiadas;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class ExemploTest {
 
